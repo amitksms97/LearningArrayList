c490e5c117753a579f8eb0530ebf9f0ece52a9f4
amitksms97 <amitksms97@gmail.com>
2021-05-01 12:07:24 +0530

    Add project files.

---
+++ LearningArrayList/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections;
+
+namespace LearningArrayList
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ArrayList list = new ArrayList();
+
+            list.Add("Some String");
+            
+            int i = 123;
+            object o = i;
+            i = (int)o;
+
+            String s = (String)list[0];
+
+        }
+    }
+}
